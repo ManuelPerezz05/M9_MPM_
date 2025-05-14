6f7cb4d61c8fde6abe71615436526792926c360e
ManuelPerez <2024_perez.maleno@iticbcn.cat>
2025-05-14 22:59:03 +0200

    Update query.php

--- classe_08_01/query.php
+++ classe_08_01/query.php
@@ -1,33 +1,17 @@
-<!--Mostra el títol, puntuació i l'any de totes les pel·lícules que siguin de l'any introduït per l'usuari-->
 <?php
-// Se obtiene el valor de un campo de formulario que se ha enviado por el método POST con el nombre 'any'.
-$any = $_POST['any'];
+session_start();
 
-// 'localhost' -> El servidor de la base de datos.
-// 'manuel' -> El nombre del usuario
-// 'manuel' -> La contraseña del usuario
-// 'base_de_datos' -> El nombre de la base de datos
-$conn = mysqli_connect("localhost", "manuel", "manuel", "base_de_datos");
+$conn = mysqli_connect('localhost', 'alexRamosExam', 'Admin1234*', 'alexRamosExam');
 
-// Verifica si la conexión fue exitosa.
 if (!$conn) {
-    echo "Error al connectar amb la BBDD";
-    exit();
-}
-
-$sql = "SELECT Titol, Any FROM Pelicules WHERE Any < $any";
-
-// Se ejecuta la consulta SQL utilizando mysqli_query()
-$query = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($query) > 0) {
-    while ($row = mysqli_fetch_array($query)) {
-        echo "Títol: " . $row['Titol'] . " - Any: " . $row['Any'] . "<br>";
-    }
+    echo "Error al conectar";
 } else {
-    // Si no se encontraron resultados, se ve el echo.
-    echo "No s'han trobat pel·lícules anteriors a l'any $any.";
+    $p = $_POST['puntuacion'];
+    $sql = "SELECT TITULO, PUNTUACION FROM peliculas WHERE puntuacion < '$p'";
+    $query = mysqli_query($conn, $sql);
+
+    while ($row = mysqli_fetch_array($query)) {
+        echo $row['TITULO'] . " - " . $row['PUNTUACION'] . "<br>";
+    }
 }
 ?>
-
-<br><a href="index.html"><button>Tornar</button></a>
